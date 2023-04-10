2e2b866ba28c51200f67ba63c74415fa3d2087c6
Bysees <bysees33@gmail.com>
2023-04-10 11:53:33 +0300

    created "vite-meta-env.d.ts" for .env typing

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="vite-plugin-svgr/client" />
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_PROXY_URL: string
-  readonly VITE_ACCESS_KEY: string
-  readonly VITE_CLIENT_ID: string
-  readonly VITE_DEVICE_ID: string
-}
---
+++ src/vite-meta-env.d.ts
@@ -0,0 +1,10 @@
+interface ImportMetaEnv {
+  readonly VITE_PROXY_HOST: string
+  readonly VITE_PROXY_PORT: string
+  readonly VITE_API_URL: string
+  readonly VITE_TOKEN_PORT: string
+  readonly VITE_SERVICE_PORT: string 
+  readonly VITE_ACCESS_KEY: string
+  readonly VITE_CLIENT_ID: string
+  readonly VITE_DEVICE_ID: string
+}
